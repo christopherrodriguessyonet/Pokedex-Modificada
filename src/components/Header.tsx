4913d6d0de5e0dfbcc67fcb5a0da60a42df83e61
christopherrodriguessyonet <christopher.rodrigues@syonet.com>
2025-01-22 22:13:37 -0300

    feat:Direciona botão batalha nova página

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,49 +1,44 @@
-import { memo } from "react"
-import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material"
-import { useNavigate, useLocation } from "react-router-dom"
+import { memo } from "react";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import { useNavigate, useLocation } from "react-router-dom";
 
-type Props = { 
-    showHomeBtn?: boolean
-    title: string
-    next?: JSX.Element
-    previus?: JSX.Element
-}
+const Component = (props) => {
+  const navigate = useNavigate();
+  const location = useLocation();
 
-const Component = (props: Props) => {
-    const navigate = useNavigate();
-    const location = useLocation(); // Obtém a rota atual
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed" color="error">
+        <Toolbar>
+          <Button
+            color="inherit"
+            onClick={() => navigate("/")}
+            variant="text"
+          >
+            HOME
+          </Button>
 
-    return (
-        <Box sx={{flexGrow: 1}}>
-            <AppBar position="fixed" color="error">
-                <Toolbar>
-                    {/* Mostrar o botão HOME em todas as páginas */}
-                    <Button color="inherit" onClick={() => navigate("/")} variant="text" >HOME</Button>
-                    
-                    {/* Mostrar o botão BATALHA somente na página principal */}
-                    {location.pathname === "/" && (
-                        <Button color="inherit" onClick={() => navigate("/batalha")} variant="text" 
-                        sx={{ marginLeft: 3 }}>BATALHA</Button>
-                    )}
+          {location.pathname === "/" && (
+            <Button
+            color="inherit"
+            onClick={() => window.location.href = "battle.html"} // Redireciona para o arquivo HTML
+            variant="text"
+            sx={{ marginLeft: 3 }}
+          >
+            BATALHA
+          </Button>
+          )}
 
-                    <Box display="flex" justifyContent="space-evenly" flexGrow={1}>
-                        {props.previus}
-                        <Typography 
-                            variant="h4"
-                            component="h1"
-                            align="center" >{props.title}</Typography>
-                        {props.next}
-                    </Box>
-                </Toolbar>
-            </AppBar>
-            <Box sx={{ marginTop: '64px' }}>
-            </Box>
-        </Box>
-    )
-}
+          <Box display="flex" justifyContent="space-evenly" flexGrow={1}>
+            <Typography variant="h4" component="h1" align="center">
+              {props.title}
+            </Typography>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box sx={{ marginTop: "64px" }}></Box>
+    </Box>
+  );
+};
 
-const propsAreEqual = (prevProps: Readonly<Props>, nextProps: Readonly<Props>) => {
-    return prevProps.title === nextProps.title;
-}
-
-export const Header = memo(Component, propsAreEqual)
+export const Header = memo(Component);
