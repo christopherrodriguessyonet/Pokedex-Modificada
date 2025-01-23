77b5d1f1c9e9aff3fd9cdb0ba49e4f8233b5392f
christopherrodriguessyonet <christopher.rodrigues@syonet.com>
2025-01-23 14:42:20 -0300

    fix:ajusta botão home quando filtro aplicado

--- src/routes/Home.page.tsx
+++ src/routes/Home.page.tsx
@@ -1,10 +1,11 @@
 import { Container, Grid, styled, Typography } from "@mui/material";
-import React from "react";
-import { Link, ScrollRestoration, useLoaderData } from "react-router-dom";
+import React, { useEffect, useCallback } from "react";
+import { Link, ScrollRestoration, useLoaderData, useLocation } from "react-router-dom";
 import { Header } from "../components/Header";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { PokemonCard } from "../components/PokemonCard";
 import { PokemonFilter } from "../components/PokemonFilter";
+import { mapPokemons } from "../services/pokemonsUtils";
 
 const CustonLink = styled(Link)`
     text-decoration: none;
@@ -14,35 +15,27 @@ const CustonLink = styled(Link)`
 
 export function HomePage() {
     const { pokemons, next } = useLoaderData() as { pokemons: any[], next: string };
-    const [pokemonList, setPokemonList] = React.useState(
-        pokemons.map((poke: any) => ({
-            name: poke.name,
-            id: parseInt(poke.url.split("/").slice(-2, -1)[0]), // Obtém o ID correto da URL
-            imageUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-                poke.url.split("/").slice(-2, -1)[0]
-            }.png`,
-        }))
-    );
+    const location = useLocation();
+
+    const [pokemonList, setPokemonList] = React.useState(mapPokemons(pokemons));
     const [nextPage, setNextPage] = React.useState(next);
+
+    // Reseta os dados iniciais ao clicar em "Home"
+    const resetToInitialState = useCallback(() => {
+        setPokemonList(mapPokemons(pokemons));
+        setNextPage(next);
+    }, [pokemons, next]);
+
+    // Detecta mudanças na rota e reseta quando necessário
+    useEffect(() => {
+        if (location.pathname === "/") {
+            resetToInitialState();
+        }
+    }, [location.pathname, resetToInitialState]);
 
     const fetchNextPage = async () => {
         const data = await fetch(nextPage).then((res) => res.json()).catch(console.error);
-
-        const pokemonsWithImages = await Promise.all(
-            data.results.map(async (poke: any) => {
-                const response = await fetch(poke.url);
-                const pokemonData = await response.json();
-
-                return {
-                    name: pokemonData.name,
-                    id: pokemonData.id, // Usa o ID correto do Pokémon
-                    imageUrl: pokemonData.sprites.front_default || 
-                              `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonData.id}.png`,
-                };
-            })
-        );
-
-        setPokemonList((prev) => [...prev, ...pokemonsWithImages]);
+        setPokemonList((prev) => [...prev, ...mapPokemons(data.results)]);
         setNextPage(data.next);
     };
 
@@ -51,6 +44,7 @@ export function HomePage() {
             <ScrollRestoration />
             <Header title="PokeDex" />
             <Container maxWidth="lg" sx={{ padding: "1.5rem" }}>
+                {/* Botões para filtrar Pokémon por tipo */}
                 <PokemonFilter setPokemonList={setPokemonList} />
                 <InfiniteScroll
                     dataLength={pokemonList.length}
@@ -62,7 +56,10 @@ export function HomePage() {
                         {pokemonList.map((item) => (
                             <Grid item key={item.id} xs={12} sm={6} md={4}>
                                 <CustonLink to={`/pokemon/${item.id}`}>
-                                    <PokemonCard pokemonName={item.name} pokemonNumber={item.id} />
+                                    <PokemonCard
+                                        pokemonName={item.name}
+                                        pokemonNumber={item.id}
+                                    />
                                 </CustonLink>
                             </Grid>
                         ))}
